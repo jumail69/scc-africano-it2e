6329a024c05243492264b7c441a54b2b736543ba
jumail69 <africanojemuel.com>
2024-08-14 17:54:32 +0800

    pls help me im under in the water

---
+++ src/africanoappe/AfricanoAppE.java
@@ -0,0 +1,15 @@
+
+package africanoappe;
+
+import java.util.Scanner;
+
+public class AfricanoAppE {
+
+   
+    public static void main(String[] args) {
+         
+       Sales sls = new Sales ();
+       sls.getSales();
+    }
+    
+}
